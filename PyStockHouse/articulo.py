1c8dd569bbb35ca1843ba1da0c288457add040c7
Francisco Quiroz <134228349+FranciscoQuirozAllende@users.noreply.github.com>
2024-01-08 19:23:00 -0300

    Se cambia el nombre al proyecto y se convierten los imports a relativos

--- PyStockHouse/articulo.py
+++ PyStockHouse/articulo.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from .auth import login_required
+from .db import get_db
 
 bp = Blueprint('articulo', __name__)
 
