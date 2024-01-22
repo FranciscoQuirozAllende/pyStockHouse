0a69bea9f731be1b20179d8fd42bb0a4f9c1a41a
FranciscoQuiroz <fg.quirozallende@gmail.com>
2024-01-22 20:17:03 -0300

    wenardo el commit

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,53 +2,65 @@ import pytest
 from flask import g, session
 from PyStockHouse.db import get_db
 
-def test_register(client,app):
-    assert client.get('/auth/register').status_code == 200
-    response = client.post(
-        '/auth/register', data={'username': 'a', 'password':'a'}
-    )
+
+def test_register(client, app):
+    assert client.get("/auth/register").status_code == 200
+    response = client.post("/auth/register", data={"username": "a", "password": "a"})
 
     assert response.headers["Location"] == "/auth/login"
 
     with app.app_context():
-        assert get_db().execute(
-            "SELECT * FROM user WHERE username = 'a'",
-        ).fetchone() is not None
+        assert (
+            get_db()
+            .execute(
+                "SELECT * FROM user WHERE username = 'a'",
+            )
+            .fetchone()
+            is not None
+        )
 
-@pytest.mark.parametrize(('username', 'password', 'message'), (
-    ('','',b'Nombre de usuario requerido'),
-    ('a','',b'Contrasena requerida'),
-    ('test', 'test',b'ya se encuentra registrado')
-))
-def test_register_validate_input(client,username,password,message):
+
+@pytest.mark.parametrize(
+    ("username", "password", "message"),
+    (
+        ("", "", b"Nombre de usuario requerido"),
+        ("a", "", b"Contrasena requerida"),
+        ("test", "test", b"ya se encuentra registrado"),
+    ),
+)
+def test_register_validate_input(client, username, password, message):
     response = client.post(
-        '/auth/register',
-        data = {'username': username, 'password': password}
+        "/auth/register", data={"username": username, "password": password}
     )
     assert message in response.data
 
-def test_login(client,auth):
-    assert client.get('/auth/login').status_code == 200
+
+def test_login(client, auth):
+    assert client.get("/auth/login").status_code == 200
     response = auth.login()
     assert response.headers["Location"] == "/"
 
     with client:
-        client.get('/')
-        assert session['user_id'] == 1
-        assert g.user['username'] == 'test'
+        client.get("/")
+        assert session["user_id"] == 1
+        assert g.user["username"] == "test"
 
-@pytest.mark.parametrize(('username', 'password','message'),(
-    ('a','test',b'Nombre de usuario incorrecto'),
-    ('test','a',b'Contrasena incorrecta'),
-))
-def test_login_validate_input(auth,username,password,message):
+
+@pytest.mark.parametrize(
+    ("username", "password", "message"),
+    (
+        ("a", "test", b"Nombre de usuario incorrecto"),
+        ("test", "a", b"Contrasena incorrecta"),
+    ),
+)
+def test_login_validate_input(auth, username, password, message):
     response = auth.login(username, password)
     assert message in response.data
 
 
-def test_logout(client,auth):
+def test_logout(client, auth):
     auth.login()
 
     with client:
         auth.logout()
-        assert 'user_id' not in session
+        assert "user_id" not in session
--- tests/test_lugar.py
+++ tests/test_lugar.py
@@ -1,42 +1,51 @@
 import pytest
 from PyStockHouse.db import get_db
 
-def test_index(client,auth):
-    response = client.get('/')
-    assert b'Iniciar Sesion' in response.data
-    assert b'Registrarse' in response.data
+
+def test_index(client, auth):
+    response = client.get("/")
+    assert b"Iniciar Sesion" in response.data
+    assert b"Registrarse" in response.data
 
     auth.login()
-    response = client.get('/')
-    assert b'Cerrar Sesion' in response.data
-    assert b'cocina' in response.data
+    response = client.get("/")
+    assert b"Cerrar Sesion" in response.data
+    assert b"cocina" in response.data
     assert b'href="/1/update"' in response.data
 
-@pytest.mark.parametrize('path',(
-    '/create',
-    '/1/update',
-    '/1/delete',
-))
-def test_login_required(client,path):
-    response = client.post(path)
-    assert response.headers['Location'] == '/auth/login'
 
-def test_author_required(app,client,auth):
+@pytest.mark.parametrize(
+    "path",
+    (
+        "/create",
+        "/1/update",
+        "/1/delete",
+    ),
+)
+def test_login_required(client, path):
+    response = client.post(path)
+    assert response.headers["Location"] == "/auth/login"
+
+
+def test_author_required(app, client, auth):
     with app.app_context():
         db = get_db()
-        db.execute('UPDATE lugar SET id_lugar=2 WHERE id_creador=1')
+        db.execute("UPDATE lugar SET id_lugar=2 WHERE id_creador=1")
         db.commit()
 
         auth.login()
-        assert client.post('/1/update').status_code == 403
-        assert client.post('/1/delete').status_code == 403
-        assert b'href="/1/update"' not in client.get('/').data
+        assert client.post("/1/update").status_code == 403
+        assert client.post("/1/delete").status_code == 403
+        assert b'href="/1/update"' not in client.get("/").data
 
-@pytest.mark.parametrize('path',(
-    '/2/update',
-    '/2/delete',
-))
-def test_exist_required(client,auth,path):
+
+@pytest.mark.parametrize(
+    "path",
+    (
+        "/2/update",
+        "/2/delete",
+    ),
+)
+def test_exist_required(client, auth, path):
     auth.login()
     assert client.post(path).status_code == 404
-
